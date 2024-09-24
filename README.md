8c4579a47e048c163bb8e1f3a1e8a4eed1638e87
LAvz <1lavz97ms@gmail.com>
2024-09-24 13:32:50 -0400

    [ADD] - endpoint to get a list of cashcards

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # simple-rest-api-with-spring-boot
+
+## Simple CRUD Rest API with Spring using TDD principles
+
+## Must reading:
+- Assertj for testing assertions: https://assertj.github.io/doc/
+- JsonPath for extracting information from response on tests: https://github.com/json-path/JsonPath
+- Spring TestRestTemplate for IT tests: https://docs.spring.io/spring-boot/api/java/org/springframework/boot/test/web/client/TestRestTemplate.html
+- RFC 9110 HTTP Semantics: https://www.rfc-editor.org/rfc/rfc9110#name-normative-references
--- src/main/java/com/example/demo/CashCardController.java
+++ src/main/java/com/example/demo/CashCardController.java
@@ -1,6 +1,9 @@
 package com.example.demo;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -28,6 +32,16 @@ class CashCardController {
         return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
+        return ResponseEntity.ok(page.getContent());
+    }
+
     @PostMapping
     private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
         CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
@@ -37,5 +51,4 @@ class CashCardController {
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
     }
-
 }
